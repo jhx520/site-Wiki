ee0ccce3999f8d3a3328fa5f33b226b94bbba03f
jhx520 <jhx520@users.noreply.github.com>
2020-03-10 17:22:59 +0000

    Automated deployment: Tue Mar 10 17:22:59 UTC 2020 ee23cfad65b5e7bb82475f17f981e1c88a83a754

--- fireworks.js
+++ fireworks.js
@@ -122,10 +122,3 @@ if (canvasEl) {
     setCanvasSize(),
     window.addEventListener("resize", setCanvasSize, !1)
 }
-
-@media (prefers-color-scheme: dark) {
-    body {
-        background-color: black;
-        color: white;
-    }
-}
